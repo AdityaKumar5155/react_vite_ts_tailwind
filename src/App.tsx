9a177a0d092da55843ad0bdf2bc8ec4ed61240a6
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-20 13:29:27 +0530

    chore : setup the repository with basic components, structure and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Hero from "./pages/Hero"
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
